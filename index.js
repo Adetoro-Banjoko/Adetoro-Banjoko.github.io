aa566ccc270ea5faa308b85a737faa49556311ff
Adetoro-Banjoko <123774166+Adetoro-Banjoko@users.noreply.github.com>
2023-02-05 10:27:03 -0600

    Add files via upload
    commit

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+// scroll functions
+var mainNav = document.querySelector('.navbar');
+window.addEventListener("scroll", (e) => {
 
+   
 
+    // add/remove class to navbar when scrolling to hide/show
+    var scroll = window.scrollTop();
+    if (scroll >= 150) {
+        mainNav.classList.add("navbar-hide");
+    } else {
+       mainNav.classList.remove("navbar-hide");
+    }
 
+});
+
+// document.querySelector(window).scroll(function(e) {
   
